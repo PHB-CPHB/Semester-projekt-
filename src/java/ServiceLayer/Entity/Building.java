228203aabca4b6819daee7992ec6a840ef5bde72
Michael <Michael@DESKTOP-NJ359SR>
2016-03-30 14:11:56 +0200

    Changes in LoginController, building and customer

--- src/java/ServiceLayer/Entity/Building.java
+++ src/java/ServiceLayer/Entity/Building.java
@@ -10,5 +10,90 @@ package ServiceLayer.Entity;
  * @author philliphbrink
  */
 public class Building {
+
+    private int buildingId;
+    private int roomId;
+    private int roomSize;
+    private String adress;
+    private String buildingName;
+    private String postal;
+    private String city;
+    private String buildingStatus;
+
+    public Building(int buildingId, int roomId, int roomSize, String adress, String buildingName, String postal, String city, String buildingStatus) {
+        this.buildingId = buildingId;
+        this.roomId = roomId;
+        this.roomSize = roomSize;
+        this.adress = adress;
+        this.buildingName = buildingName;
+        this.postal = postal;
+        this.city = city;
+        this.buildingStatus = buildingStatus;
+    }
+
+    public int getBuildingId() {
+        return buildingId;
+    }
+
+    public void setBuildingId(int buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getBuildingStatus() {
+        return buildingStatus;
+    }
+
+    public void setBuildingStatus(String buildingStatus) {
+        this.buildingStatus = buildingStatus;
+    }
+
+    public int getRoomSize() {
+        return roomSize;
+    }
+
+    public void setRoomSize(int roomSize) {
+        this.roomSize = roomSize;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     
 }
---
+++ src/java/ServiceLayer/LoginController.java
@@ -0,0 +1,133 @@
+package ServiceLayer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import DataAccessLayer.DBConnector;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author philliphbrink
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            //Connect to the database
+            ResultSet rs = null;
+            Statement statement = null;
+            Connection connection = null;
+            Class.forName(DBConnector.driver);
+            connection = DriverManager.getConnection(DBConnector.URL, DBConnector.ID, DBConnector.ID);
+            statement = connection.createStatement();
+            //Setup for username and password
+            String Username = (String) request.getParameter("Username");
+            String Password = (String) request.getParameter("Password");
+            String nextJSP = "Cusadd.jsp";
+            String doThis = request.getParameter("doThis");
+            doThis = doThis.toLowerCase();
+            String Query;
+            String Role = null;
+            //check if username and password maches
+            switch(doThis) {
+                case "doThis":
+                    Query = "Select cusername and cpassword from customer";  //insert from database!!
+                    rs = statement.executeQuery(Query);
+                    while(rs.next()) {
+                        Role = rs.getString(1);
+                    }
+                    if("Admin".equals(Role)) {
+                        nextJSP = "Cusadd.jsp";
+                    } else if ("Customer".equals(Role)){
+                        nextJSP = "Cusadd.jsp";
+                    } else {
+                        nextJSP = "Cusadd.jsp";
+                    }
+            }
+            response.sendRedirect(nextJSP);
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
