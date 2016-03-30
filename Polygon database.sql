f82511c52539cf3ee96423d42ecbaeb18cd63ae5
ovillumsen <ovillumsen@hotmail.dk>
2016-03-30 13:28:43 +0200

    Added files via upload

--- Polygon database.sql
+++ Polygon database.sql
@@ -6,14 +6,13 @@ CREATE DATABASE polygon;
 USE polygon;
 
 CREATE TABLE buildings(
-Building_name INT(10) NOT NULL,
-Building_status VARCHAR(10),
-Room_id INT(10) NOT NULL,
-Building_type VARCHAR(10),
-adress VARCHAR(10),
+building_name VARCHAR(45),
+building_status VARCHAR(145),
+building_type VARCHAR(145),
+adress VARCHAR(45),
 year INT(10),
 postnumber INT(10),
-city VARCHAR(10),
+city VARCHAR(45),
 areasize INT(10)
 );
 
@@ -27,3 +26,13 @@ cusername VARCHAR(10),
 cpassword VARCHAR(10),
 customer_id INT(10)
 );
+
+INSERT INTO buildings (building_name, building_status, building_type, adress, year, postnumber, city, areasize) VALUES ('Olivers bygning', 'I stand', 'Hus', 'Kappa 123', 1978, 3240, 'Metrocity', 70);
+
+INSERT INTO admin (ausername, apassword) VALUES ('Oliver', '123');
+INSERT INTO admin (ausername, apassword) VALUES ('Tim', '1234');
+INSERT INTO admin (ausername, apassword) VALUES ('Phillip', '12345');
+INSERT INTO admin (ausername, apassword) VALUES ('Michael', '123456');
+
+INSERT INTO customer (cusername, cpassword, customer_id) VALUES ('bob', 'bob', 1);
+INSERT INTO customer (cusername, cpassword, customer_id) VALUES ('Kaj', 'Kaj', 2);
