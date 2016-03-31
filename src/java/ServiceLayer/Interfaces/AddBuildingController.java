0780566d60a80cb1eb45eba197637d2ee3fd8160
michael2750 <2750daugbjerg@gmail.com>
2016-03-31 11:10:07 +0200

    Added servlet AddBuildController

---
+++ src/java/ServiceLayer/Interfaces/AddBuildingController.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServiceLayer.Interfaces;
+
+import DataAccessLayer.DBConnector;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ejer
+ */
+@WebServlet(name = "AddBuildingController", urlPatterns = {"/AddBuildingController"})
+public class AddBuildingController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            DBConnector.getConnection();
+            ResultSet rs = null;
+            Statement statement = null;
+            Connection connection = null;
+            String Query;
+            String nextJSP = null;
+            String doThis = request.getParameter("doThis");
+            doThis = doThis.toLowerCase();
+            statement = connection.createStatement();
+            String building_name = (String) request.getParameter("name");
+            String building_ = (String) request.getParameter("name");
+            String building_name = (String) request.getParameter("name");
+            String building_name = (String) request.getParameter("name");
+            switch (doThis) {
+                case "doThis":
+                    Query = "select ausername, apassword from admin where ausername ="+ Username + "  and apassword =" + Password;  //insert from database!!
+                    rs = statement.executeQuery(Query);
+                    if("Building".equals(rs)){
+                        
+                    }
+                    
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
