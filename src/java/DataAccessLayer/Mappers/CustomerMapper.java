d85d8ba5568815d49e372e7a2e159806aeb187cf
michael2750 <2750daugbjerg@gmail.com>
2016-04-04 12:26:18 +0200

    Made method createCustomer in CustomerMapper

--- src/java/DataAccessLayer/Mappers/CustomerMapper.java
+++ src/java/DataAccessLayer/Mappers/CustomerMapper.java
@@ -18,19 +18,34 @@ import java.util.logging.Logger;
  * @author philliphbrink
  */
 public class CustomerMapper {
-    
+
     public int getCustomer(String customer_name) {
         int customer_id = 0;
         try {
             PreparedStatement pstmt = DBConnector.getConnection().prepareStatement("SELECT * FROM customer WHERE customer_name = ?");
             pstmt.setString(1, customer_name);
             ResultSet rs = pstmt.executeQuery();
-                rs.next();
-              customer_id = rs.getInt("customer_id");
+            rs.next();
+            customer_id = rs.getInt("customer_id");
         } catch (SQLException ex) {
             Logger.getLogger(CustomerMapper.class.getName()).log(Level.SEVERE, null, ex);
-        }  
-        return customer_id;
         }
+        return customer_id;
     }
+    
+    public boolean createCustomer(String username, String password, String user_role){
+        try{
+            PreparedStatement pstmt = DBConnector.getConnection().prepareStatement("INSERT INTO login (username, password, user_role) VALUES ('?', '?', ?, '?')");
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setString(4, user_role);
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
+
 
